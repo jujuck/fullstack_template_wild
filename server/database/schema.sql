9649d16ab07509749e47056bd3641e9049ae8e02
Megane <megane.loisele@gmail.com>
2024-06-17 11:15:17 +0200

    SQL mis en place

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,4 +1,21 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+CREATE TABLE status (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  status VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE tasks (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  id_status INT,
+  task VARCHAR(255) NOT NULL,
+  deadline DATE NOT NULL,
+  who VARCHAR(50) NOT NULL,
+  FOREIGN KEY (id_status) REFERENCES status(id)
+);
+
+INSERT INTO status (status) VALUES ('Done');
+INSERT INTO status (status) VALUES ('ToDo');
+INSERT INTO status (status) VALUES ('Pending');
+
+INSERT INTO tasks (id_status,task, deadline, who) VALUES (1,'wash the dishes', '2024-06-19', 'Huguette');
+INSERT INTO tasks (id_status,task, deadline, who) VALUES (2,'wash the laundry', '2024-06-17', 'Jean-Michel');
+INSERT INTO tasks (id_status,task, deadline, who) VALUES (3,'folding laundry', '2024-06-17', 'Nicolas');
